f928a54ac556edf59ebc0d40eaee2b2758caed1c
Yongjoo Park <pyongjoo@umich.edu>
2012-03-29 22:04:24 -0400

    java home added in script

--- mysrc/invIndexTest.sh
+++ mysrc/invIndexTest.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+export JAVA_HOME=/usr
+
 ../bin/hadoop jar InvertedIndex.jar edu.umich.cse.pyongjoo.InvertedIndex ../dataset/test output
--- mysrc/wordCountTest.sh
+++ mysrc/wordCountTest.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export JAVA_HOME=/usr
 ../bin/hadoop jar WordCount.jar edu.umich.cse.pyongjoo.WordCount ../dataset/test output
